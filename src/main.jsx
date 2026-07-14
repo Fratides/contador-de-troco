b90a77b5af7f5a78bc2ac2e8bcf6c3ead69661f8
Fratides <fratides@gmail.com>
2026-07-13 23:12:28 -0300

    Atualizaçao para o github

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,9 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import './index.css'
-import { registerServiceWorker } from './registerServiceWorker'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 )
-
-registerServiceWorker()
